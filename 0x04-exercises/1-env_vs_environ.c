5f4e051d774851c3523594433d7ac4c92cc4719f
CarolinaHV <carolina.hv02@gmail.com>
2020-11-18 17:11:43 +0000

    Difencent between env and environ.0

--- 0x04-exercises/1-env_vs_environ.c
+++ 0x04-exercises/1-env_vs_environ.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main -  Argument env vs Global variable environ
+ * @ac: No used
+ * @av: No used
+ * @env: path the environt
+ * Description - It is verified that there is no difference between
+ * the third argument and the global variable by checking the address
+ * of each one.
+ * Return: Value 0
+ */
 
 extern char **environ;
 
